ba034401ad4dc80665df3e739faf0c41ccb12431
Matthew Richards <mrrichards15@gmail.com>
2023-07-12 21:14:53 -0400

    Temp fix for match

--- DraftPickValues.py
+++ DraftPickValues.py
@@ -43,32 +43,33 @@ pick_value_round_13 = 235
 #pick_value_round_13 = 237
 
 def get_draft_pick_value(draft_pick):
-    match draft_pick:
-        case "1":
-            return pick_value_round_1
-        case "2":
-            return pick_value_round_2
-        case "3":
-            return pick_value_round_3
-        case "4":
-            return pick_value_round_4
-        case "5":
-            return pick_value_round_5
-        case "6":
-            return pick_value_round_6
-        case "7":
-            return pick_value_round_7
-        case "8":
-            return pick_value_round_8
-        case "9":
-            return pick_value_round_9
-        case "10":
-            return pick_value_round_10
-        case "11":
-            return pick_value_round_11
-        case "12":
-            return pick_value_round_12
-        case "13":
-            return pick_value_round_13
-        case _:
-            return 0
+    return 10
+    #match draft_pick:
+    #    case "1":
+    #        return pick_value_round_1
+    #    case "2":
+    #        return pick_value_round_2
+    #    case "3":
+    #        return pick_value_round_3
+    #    case "4":
+    #        return pick_value_round_4
+    #    case "5":
+    #        return pick_value_round_5
+    #    case "6":
+    #        return pick_value_round_6
+    #    case "7":
+    #        return pick_value_round_7
+    #    case "8":
+    #        return pick_value_round_8
+    #    case "9":
+    #        return pick_value_round_9
+    #    case "10":
+    #        return pick_value_round_10
+    #    case "11":
+    #        return pick_value_round_11
+    #    case "12":
+    #        return pick_value_round_12
+    #    case "13":
+    #        return pick_value_round_13
+    #    case _:
+    #        return 0
